3c908069211f5af650369788a57d93e27d68ae53
Logan Saso <logansaso@gmail.com>
2023-08-09 00:00:13 -0700

    Bump version

--- ProgressTogether/ProgressTogether.cs
+++ ProgressTogether/ProgressTogether.cs
@@ -22,7 +22,7 @@ public class ProgressTogether : TerrariaPlugin
         "Blocks bosses that haven't been spawned yet until enough of your friends are online!";
 
     public override string Name => Log.Name;
-    public override Version Version => new Version(0, 0, 2, 0);
+    public override Version Version => new(0, 0, 3, 0);
 
     public override void Initialize()
     {
